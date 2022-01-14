9e602bb06b163295c99ca13bfe3d49ce96621d0f
turguteyupcan <turguteyupcan@gmail.com>
2022-01-15 00:35:11 +0300

    Proje eklendi

---
+++ UnluCoBootCamp.Odev1.Api/UnluCoBootCamp.Odev1.Api/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿namespace UnluCoBootCamp.Odev1.Api.Models
+{
+    public class Animal
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Tur { get; set; }
+
+        public string KlubeNo { get; set; }
+    }
+}
